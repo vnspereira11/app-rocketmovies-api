4da3ebcf8c39c1f0d22956ca00d4a1bd524a69d6
Vanessa Pereira <vnspereira11@gmail.com>
2023-01-19 14:36:08 -0300

    Create users routes, AppError and database

--- src/server.js
+++ src/server.js
@@ -1,11 +1,33 @@
+require("express-async-errors");
+const database = require("./database/sqlite");
+const AppError = require("./utils/AppError");
+
 const express = require("express");
+
+const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.send("Hello, dev front-end em 2023!")
-});
+app.use(routes);
+
+database();
+
+app.use( (error, request, response, next) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: "error",
+        message: error.message
+      });
+    };
+    
+    console.log(error);
+  
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error"
+    });
+  });
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
