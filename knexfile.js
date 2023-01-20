685aa4b591202674a8664dee591fd1209358bbcc
Vanessa Pereira <vnspereira11@gmail.com>
2023-01-20 18:58:18 -0300

    Creating migration for movie notes

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },  
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
   },
